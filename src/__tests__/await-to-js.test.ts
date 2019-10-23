2557338d59aa3f81746b3545710133f84a54a6b9
Wallop <walloped@protonmail.com>
2019-10-22 20:51:13 -0500

    merge tests from last commit
    strict: false required to fix one test - cba to fix rn

--- src/__tests__/await-to-js.test.ts
+++ src/__tests__/await-to-js.test.ts
@@ -1,4 +1,4 @@
-import { to, toAll } from '../await-to-js';
+import { to, toAll, promiseVars } from '../await-to-js';
 
 describe('Await to test', () => {
   it('should return a value when resolved', async () => {
@@ -43,6 +43,48 @@ describe('Await to test', () => {
   });
 });
 
+describe('promiseVars', async () => {
+  it('should return a value when resolved', async () => {
+    const testInput = 41;
+    const promise = Promise.resolve(testInput);
+
+    const [err, data] = await promiseVars<number>(promise);
+
+    expect(err).toBeNull();
+    expect(data).toEqual(testInput);
+  });
+
+  it('should return an error when promise is rejected', async () => {
+    const testInput = 41;
+    const promise = Promise.reject('Error');
+
+    const [err, data] = await promiseVars<number>(promise);
+
+    expect(err).toEqual('Error');
+    expect(data).toBeUndefined();
+  });
+
+  it('should receive the type of the parent if no type was passed', async () => {
+    let user: { name: string };
+    let err: Error;
+
+    [err, user] = await promiseVars(Promise.resolve({ name: '123' }));
+
+    expect(user.name).toEqual('123');
+  });
+
+  it('should NOT add external properties to the error object', async () => {
+    const promise = Promise.reject({ error: 'Error message' });
+
+    const [err] = await promiseVars<string, { error: string; extraKey: number }>(promise, {
+      extraKey: 1
+    });
+
+    expect(err).toBeTruthy();
+    expect((err as any).extraKey).toBeUndefined();
+    expect((err as any).error).toEqual('Error message');
+  });
+});
 describe('Await toAll test', () => {
   it('should be able to handle multiple promises', async () => {
     const promiseOne = Promise.resolve(0);
@@ -54,7 +96,6 @@ describe('Await toAll test', () => {
     expect(err).toBeNull();
     expect(data).toEqual([0, 1, 'string']);
   });
-
 
   it('should return error if not all promises resolve', async () => {
     const promiseOne = Promise.resolve(0);
@@ -75,7 +116,9 @@ describe('Await toAll test', () => {
       ext: number;
     }
 
-    const [error] = await toAll<number, number, CustomError>([promiseOne, promiseThree], { ext: 2 });
+    const [error] = await toAll<number, number, CustomError>([promiseOne, promiseThree], {
+      ext: 2
+    });
 
     if (error !== null) {
       expect(error.ext).toEqual(2);
